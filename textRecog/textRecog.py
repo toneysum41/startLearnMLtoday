38e8c6570972e75a923b13496150f3257dbc1b26
terrytong <toneysum41@gmail.com>
2019-05-24 19:58:19 +0800

    5/24update

--- textRecog/textRecog.py
+++ textRecog/textRecog.py
@@ -8,6 +8,9 @@ path = 'C:\\Users\\toney\\PycharmProjects\\tensorflowDemo\\MNIST_data'
 weightIH = [[[0]*10 for _ in range(748)] for _ in range(100)]
 weightHO = [[[0]*10 for _ in range(100)] for _ in range(10)]
 
+"""
+分别读取训练数据和测试数据，两个文件都是来自于mnist提供的数据文件
+"""
 def load_mnist_train(path, kind='train'):
     labels_path = os.path.join(path, '%s-labels.idx1-ubyte' % kind)
     images_path = os.path.join(path, '%s-images.idx3-ubyte' % kind)
@@ -31,11 +34,16 @@ def load_mnist_test(path, kind='t10k'):
         images = np.fromfile(imgpath, dtype=np.uint8).reshape(len(labels), 784)
     return images, labels
 
-def recogChar(num, network, test_images):
+"""
+我自己定义最后的执行识别的函数：将测试数据集循环放入神经网络的query中进行判定，每组结果中数值最大的判定为识别结果
+最后再算一个总体识别概率
+"""
+def recogChar(num, network, test_images, test_labels):
         temp = 0.0
         count = 0
         number = 0
         round = 1
+        accurate = 0
         for i in test_images:
             result = network.query((np.asfarray(i) / 255) + 0.01)
             for r in result:
@@ -43,53 +51,45 @@ def recogChar(num, network, test_images):
                     temp = r
                     number = count
                 count += 1
-            print('Round', round, 'The number ', number, 'is found')
+            print('Round', round, 'The number ', number, 'is found', '(It should be', test_labels[round-1], ')')
+            if number == test_labels[round-1]:
+                accurate += 1
             round += 1
             count = 0
             temp = 0.0
             number = 0
+        print('The accuracy is : ', (accurate/len(test_images))*100, '%')
 
 
 
-
+"""
+创建一个训练函数，使用训练数据集对你所创建的神经网络进行训练，这里是使用的10000次的训练数据
+最终得到的就是所需要的带有优化过的权重值的神经网络，在这其中还可以尝试将训练后的权重结果进行
+保存，使你训练的神经网络得以延续使用，例如在其他project中调用
+"""
 def buildTrainingNetwork(input_nodes, hidden_nodes, output_nodes, learning_rate):
     network = wordsrecog.neuralNetwork(input_nodes, hidden_nodes, output_nodes, learning_rate)
     train_images, train_labels = load_mnist_train(path)
     #for j in range(10):
+    counter = 0
     for i in train_images:
             # img = i.reshape(28, 28)
             scaledinput_images = (np.asfarray(i) / 255.0 * 0.99) + 0.01
             onodes = 10
             targets = np.zeros(onodes) + 0.01
-            targets[7] = 0.99
+            targets[train_labels[counter]] = 0.99
             network.train(scaledinput_images, targets)
+            counter += 1
+
        # weightIH[j] = network.wih
        # weightHO[j] = network.who
     test_images, test_labels = load_mnist_test(path)
-    recogChar(9, network, test_images)
+    recogChar(9, network, test_images, test_labels)
 
 
 buildTrainingNetwork(784, 100, 10, 0.2)
 
-#print(targets)
 
-# fig, ax = plt.subplots(
-#     nrows=10,
-#     ncols=10,
-#     sharex=True,
-#     sharey=True, )
-#
-# ax = ax.flatten()
-# count = 0
-# for j in range(10):
-#     for i in range(10):
-#         img = train_images[train_labels == i][j].reshape(28, 28)
-#         ax[count].imshow(img, cmap='Greys', interpolation='nearest')
-#         count += 1
-#     ax[j].set_xticks([])
-#     ax[j].set_yticks([])
-# plt.tight_layout()
-# plt.show()
 
 
 
--- textRecog/wordsrecog.py
+++ textRecog/wordsrecog.py
@@ -5,6 +5,11 @@ import tensorflow as ts
 
 
 class neuralNetwork(object):
+    """
+    初始化函数，初始化输入节点，输出节点和含隐层节点
+    再之后定义，输入层到隐藏层的权重和隐藏层到输出层的权重，并且赋予随机初始值
+    最后定义学习率变量
+    """
     def __init__(self, inputnodes, hiddennodes, outputnodes, learningrate):
         self.inodes = inputnodes
         self.onodes = outputnodes
@@ -13,9 +18,20 @@ class neuralNetwork(object):
         self.wih = numpy.random.normal(0.0, pow(self.hnodes, -0.5), (self.hnodes, self.inodes))
         self.who = numpy.random.normal(0.0, pow(self.onodes, -0.5), (self.onodes, self.hnodes))
         self.lr = learningrate
+        """
+        定义激活函数，激活函数主要解决了解决线性不可分的问题，不同激活函数的选取，激活函数种类很多，随着神经网络的优化，
+        激活函数的适应性越来越高, 我这里主要来尝试传统sigmoid函数（书上提供的），和Relu函数及其变体=》根据查资料主要是
+        为了解决sigmoid函数的梯度消失问题
+        """
         self.activation_function = lambda x: scipy.special.expit(x)
+        #self.activation_function = lambda x: ts.nn.relu(x)
+        #self.activation_function = lambda x: ts.nn.crelu(x)
+        #self.activation_function = lambda x: ts.nn.leaky_relu(x)
         pass
 
+    """
+     训练函数：处理训练数据，对整体网络的各级权重进行训练，寻找最优值
+    """
     def train(self, inputs_list, targets_list):
         inputs = numpy.array(inputs_list, ndmin=2).T
         targets = numpy.array(targets_list, ndmin=2).T
@@ -30,6 +46,9 @@ class neuralNetwork(object):
         self.wih += self.lr * numpy.dot(hidden_errors * hidden_outputs * (1 - hidden_outputs), numpy.transpose(inputs))
         pass
 
+    """
+      执行函数：在网络训练后进行实际数据的检测
+     """
     def query(self, inputs_list):
         inputs = numpy.array(inputs_list, ndmin=2).T
         hidden_inputs = numpy.dot(self.wih, inputs)
